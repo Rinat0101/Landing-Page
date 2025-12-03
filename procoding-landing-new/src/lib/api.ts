443f8125ff3b481e043aa2cb6d999e38e743b4cf
Anastasiia <anastasiaaaguseva@gmail.com>
2025-12-04 01:53:29 +0300

    wp

--- procoding-landing-new/src/lib/api.ts
+++ procoding-landing-new/src/lib/api.ts
@@ -1,14 +1,16 @@
 // lib/api.ts
 export async function getCourseBySlug(slug: string) {
-    const res = await fetch(`http://procoding-headless.local/wp-json/wp/v2/course?slug=web-development`, {
+  const res = await fetch(
+    `https://docker-image-production-b2d9.up.railway.app/wp-json/wp/v2/course?slug=${slug}`,
+    {
       next: { revalidate: 60 },
-    });
-  
-    if (!res.ok) {
-      throw new Error(`Failed to fetch course: ${res.statusText}`);
     }
-  
-    const data = await res.json();
-    console.log(data)
-    return data[0];
+  );
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch course page: ${res.statusText}`);
   }
+
+  const data = await res.json();
+  return data[0];
+}
