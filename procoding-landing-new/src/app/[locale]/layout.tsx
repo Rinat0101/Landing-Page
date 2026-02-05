9498813ffbb244b2e7bd4d88002fcb8810b91a8a
Anastasiia <anastasiaaaguseva@gmail.com>
2026-02-05 21:55:22 +0300

    google tag manager

--- procoding-landing-new/src/app/[locale]/layout.tsx
+++ procoding-landing-new/src/app/[locale]/layout.tsx
@@ -1,4 +1,3 @@
-// app/[locale]/layout.tsx
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import Script from "next/script";
@@ -31,11 +30,33 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <head>
+        {/* ✅ Google Tag Manager (in head) */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5NWZTQMH');
+          `}
+        </Script>
+      </head>
       <body
         suppressHydrationWarning
         className={`${openSans.variable} font-sans bg-white text-black dark:bg-black dark:text-white`}
       >
-        {/* 🟣 Meta (Facebook Pixel) */}
+        {/* ✅ Google Tag Manager (noscript fallback in body) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5NWZTQMH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {/* 🟣 Facebook Pixel */}
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
